da2c9266bf8cffbc2901fb58bc7b2e3968c61382
wuxiaolei <wuxiaolei@megvii.com>
2024-12-30 17:36:58 +0800

    feat: update关键生成脚本.sh

--- scripts/inference_e2e_for_vlm_template.sh
+++ scripts/inference_e2e_for_vlm_template.sh
@@ -9,7 +9,6 @@ fi
 # 获取输入整数
 N=$1
 SCENE_JSON=$2
-REF_JSON=$3
 echo "total gpus "$N""
 
 
@@ -26,7 +25,7 @@ echo $current_date
 
 # 遍历小于 N 的所有整数，并行运行 Python 脚本
 for i in $(seq 0 $((N-1))); do
-  CUDA_VISIBLE_DEVICES=$i python run_inference_e2e_for_vlm_template.py --rank_id "$i" --world_size "$N" --scene_json "$SCENE_JSON" &  # 将当前整数传递给 Python 脚本
+  CUDA_VISIBLE_DEVICES=$i python run_inference_e2e_for_vlm_template.py --rank_id "$i" --world_size "$N" --scene_json "$SCENE_JSON" --save_local &  # 将当前整数传递给 Python 脚本
 done
 
 wait
